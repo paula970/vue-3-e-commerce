c2216930bfd8e6ea42e7bcbf820d4d8e26115342
paula970 <paula.maldonado@guane.com.co>
2025-07-22 15:04:00 -0500

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+// .eslintrc.cjs
+export default {
+  root: true,
+  env: {
+    node: true,
+    'vue/setup-compiler-macros': true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/prettier', // ¡Importante para la integración con Prettier!
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // Puedes añadir tus reglas personalizadas aquí
+  },
+}
---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+// .prettierrc.cjs
+module.exports = {
+  semi: true, // Puntos y coma al final de las declaraciones
+  trailingComma: 'es5', // Comas al final de objetos, arrays, etc.
+  singleQuote: true, // Usar comillas simples en lugar de dobles
+  printWidth: 100, // Ancho máximo de línea antes de envolver
+  tabWidth: 2, // Número de espacios por indentación
+}
